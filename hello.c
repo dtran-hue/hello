e6b3f5585023b9c1f2a554e88c46509fcedeabb0
Daniel Tran <danieltran1127@gmail.com>
2025-08-28 09:46:54 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Daniel Tran
+ * Email: danieltran1127@gmail.com
+ * Date: 2025/08/28
+ *
+ * My name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Daniel Tran\n");
+}
+
+
